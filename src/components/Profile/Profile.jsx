cf0685ff6cab5ebb34b3455199b01f43edd91ad1
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-08 13:19:32 +0300

    start5

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,12 +8,27 @@ const Profile = (props) => {
 
   return (
     <div className={s.wrapper}>
-      <div className="profile">
-        <div className="description">
-          <img src={avatar} alt={username} className="avatar" />
-          <p className={username}>{username}</p>
-          <p className={location}>{location}</p>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <img src={avatar} alt={username} className={s.avatar} />
+          <p className={s.username}>{username}</p>
+          <p className={s.tag}>@{tag}</p>
+          <p className={s.location}>{location}</p>
         </div>
+        <ul className={s.stats}>
+          <li className={s.item}>
+            <span className={s.label}>Followers</span>
+            <span className={s.quantity}>{stats.followers}</span>
+          </li>
+          <li className={s.item}>
+            <span className={s.label}>Views</span>
+            <span className={s.quantity}>{stats.views}</span>
+          </li>
+          <li className={s.item}>
+            <span className={s.label}>Likes</span>
+            <span className={s.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
@@ -24,7 +39,13 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
+  stats: PropTypes.objectOf(
+    PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired
+  ),
 };
 
 export default Profile;
