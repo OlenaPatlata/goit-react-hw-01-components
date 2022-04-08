843e0302a52f1bbef335a5f5c3f42b6f5f1236d1
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-08 11:55:47 +0300

    start3

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,12 +5,11 @@ import user from "db/user";
 // import { user } from '/user';
 
 const Main = () => {
-  console.log(user.tag);
   return (
     <>
       <div className={s.wrapper}>
         <Profile
-          usermame={user.username}
+          username={user.username}
           tag={user.tag}
           location={user.location}
           avatar={user.avatar}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,21 +3,14 @@ import user from "db/user";
 import s from "./Profile.module.css";
 import PropTypes from "prop-types";
 
-console.log(user);
-
 const Profile = (props) => {
-  console.log(props);
   const { username, tag, location, avatar, stats } = props;
 
   return (
     <div className={s.wrapper}>
       <div className="profile">
         <div className="description">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-            alt="User avatar"
-            className="avatar"
-          />
+          <img src={avatar} alt={username} className="avatar" />
           <p className={username}>{username}</p>
           <p className={location}>{location}</p>
         </div>
@@ -26,7 +19,7 @@ const Profile = (props) => {
   );
 };
 
-Profile.PropTypes = {
+Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
