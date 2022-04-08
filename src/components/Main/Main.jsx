49d535d09ed8b2e4993133d456a9de5867fa75a5
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-08 10:52:24 +0300

    start2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,21 @@
-import Profile from "components/Profile/Profile";
 import s from "./Main.module.css";
+import Profile from "components/Profile/Profile";
+
+import user from "db/user";
+// import { user } from '/user';
 
 const Main = () => {
+  console.log(user.tag);
   return (
     <>
       <div className={s.wrapper}>
-        <Profile />
+        <Profile
+          usermame={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
       </div>
     </>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,37 @@
 import user from "db/user";
-// import { user } from "/user";
+
 import s from "./Profile.module.css";
+import PropTypes from "prop-types";
+
+console.log(user);
 
 const Profile = (props) => {
   console.log(props);
+  const { username, tag, location, avatar, stats } = props;
+
   return (
     <div className={s.wrapper}>
-      <p>ffffffff</p>
-      <p></p>
+      <div className="profile">
+        <div className="description">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+            alt="User avatar"
+            className="avatar"
+          />
+          <p className={username}>{username}</p>
+          <p className={location}>{location}</p>
+        </div>
+      </div>
     </div>
   );
+};
+
+Profile.PropTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
 };
 
 export default Profile;
