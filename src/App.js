89b032a0bcb201112a9f888d5a252d95c40c245b
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-08 14:18:37 +0300

    start6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Main from "./components/Main/Main";
+// import style from "./components/App.css";
 
 function App() {
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,28 +8,26 @@ const Profile = (props) => {
 
   return (
     <div className={s.wrapper}>
-      <div className={s.profile}>
-        <div className={s.description}>
-          <img src={avatar} alt={username} className={s.avatar} />
-          <p className={s.username}>{username}</p>
-          <p className={s.tag}>@{tag}</p>
-          <p className={s.location}>{location}</p>
-        </div>
-        <ul className={s.stats}>
-          <li className={s.item}>
-            <span className={s.label}>Followers</span>
-            <span className={s.quantity}>{stats.followers}</span>
-          </li>
-          <li className={s.item}>
-            <span className={s.label}>Views</span>
-            <span className={s.quantity}>{stats.views}</span>
-          </li>
-          <li className={s.item}>
-            <span className={s.label}>Likes</span>
-            <span className={s.quantity}>{stats.likes}</span>
-          </li>
-        </ul>
+      <div className={s.description}>
+        <img src={avatar} alt={username} className={s.avatar} />
+        <p className={s.username}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
     </div>
   );
 };
